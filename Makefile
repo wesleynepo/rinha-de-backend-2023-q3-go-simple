5547681e06d5007f5db74a1db48ad1e240571952
wesleynepo <wesleynepomuceno1@gmail.com>
2023-08-19 12:15:22 -0300

    feat: create dockerfile

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 .PHONY: run/api 
 run/api:
 	go run ./cmd/api
+
+.PHONY: build/api
+build/api:
+	@echo 'Building cmd/api...'
+	go build -ldflags='-s -X main.buildTime=${current_time}' -o=./bin/api ./cmd/api
+
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,7 +36,7 @@ func main() {
 
     flag.IntVar(&cfg.port, "port", 4000, "API server port")
     flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-    flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+    flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
 
     flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
     flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
